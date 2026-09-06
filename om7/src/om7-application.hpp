9f52da22c196e47a65c6897fb274107ef569819a
nands-aco <nands-aco@users.noreply.github.com>
2026-09-06 23:30:18 +0900

    共有のためにコミットします。

--- om7/src/om7-application.hpp
+++ om7/src/om7-application.hpp
@@ -28,10 +28,13 @@ namespace om7
 	// ※プラットフォームごとに定義を調整する可能性があります。
 	struct Om7GraphPixel
 	{
-		std::uint16_t R : 4;
-		std::uint16_t G : 4;
-		std::uint16_t B : 4;
+		Om7GraphPixel() : R(0), G(0), B(0), A(15) {}
+		Om7GraphPixel(std::uint16_t r, std::uint16_t g, std::uint16_t b) : R(r), G(g), B(b), A(15) {}
+		Om7GraphPixel(std::uint16_t r, std::uint16_t g, std::uint16_t b, std::uint16_t a) : R(r), G(g), B(b), A(a) {}
 		std::uint16_t A : 4;
+		std::uint16_t B : 4;
+		std::uint16_t G : 4;
+		std::uint16_t R : 4;
 	};
 	static_assert(sizeof(Om7GraphPixel) == 2);
 
@@ -47,6 +50,15 @@ namespace om7
 		virtual void OnInit();
 		virtual void OnTerm();
 		virtual void OnUpdate() noexcept {}
-		virtual void OnGraphRender(const std::int32_t width, const std::int32_t height, Om7GraphPixel buffer[]) noexcept {}
+		virtual void OnGraphRender(const std::int32_t width, const std::int32_t height, Om7GraphPixel buffer[]) noexcept
+		{
+			for (std::int32_t r = 0; r < height; ++r)
+			{
+				for (std::int32_t c = 0; c < width; ++c)
+				{
+					buffer[r * width + c] = Om7GraphPixel(0, 0, 15, 15);
+				}
+			}
+		}
 	};
 }
